1fb2f0119cf351c733c8902da2e534539823e1de
Julien Schenevey <julien.schenevey@gmail.com>
2018-03-01 21:49:25 +0100

    - try avoid impossible ride

--- HashCode/Algo/JSY.cs
+++ HashCode/Algo/JSY.cs
@@ -20,20 +20,23 @@ namespace HashCode.Algo
 
         private static void AssignRide(Simulation sim)
         {
+            var remaining = sim.Steps - sim.CurrentStep;
             foreach (var vehicle in sim.Vehicules.Where(v => v.CurrentRide == null))
             {
                 var bestRide = sim.Rides.FirstOrDefault();
                 var bestDistance = long.MaxValue;
                 var bestReward = long.MinValue;
+                var bestScore = double.MinValue;
                 foreach (var ride in sim.Rides)
                 {
                     var distance = vehicle.GetDistanceTo(ride.StartPoint);
-                    var reward = ride.GetDistance() +
-                                 (sim.Steps - sim.CurrentStep - ride.GetDistance() > 0 ? sim.Bonus : 0);
-                    if (bestDistance <= distance && bestReward <= reward)
+                    var reward = ride.GetDistance() + (remaining - ride.GetDistance() > 0 ? sim.Bonus : 0);
+                    var score = distance * 0.5 + reward * 0.5;
+                    if (score <= bestScore && distance + reward <= remaining)
                         continue;
                     bestDistance = distance;
                     bestReward = reward;
+                    bestScore = score;
                     bestRide = ride;
                 }
 
